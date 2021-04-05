df616c57f7de416f053b119ff9a382ede46b4433
Daniel Tolentino <danieltolentino56@gmail.com>
2021-04-05 14:21:15 -0300

    adiciona comentários e limpa console logs comentados

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
 //API para consultar valor do dólar via fetch
 const URL_TO_FETCH = 'https://economia.awesomeapi.com.br/json/all/USD-BRL,EUR-BRL,BTC-BRL';
 fetch(URL_TO_FETCH, {
-  method: 'get' // opcional
+  method: 'get' //required
 })
 .then(function(response) {
   
@@ -11,8 +11,7 @@ fetch(URL_TO_FETCH, {
      dolarHoje = result.USD["ask"]
      eurHoje = result.EUR["ask"]
      btcHoje = result.BTC["ask"]
-        /* var json = JSON.parse(dolarHoje)
-        console.log(json) */
+  
     })
 })
 .catch(function(err) { 
@@ -26,11 +25,13 @@ var moeda = document.getElementById('entrada').value
 //converte o valor digitado para um número Float, com cadas decimais
 parseFloat(moeda)
 
-//multiplica o valor da variável 'moeda', deixa 2 casas decimais fixas, e substitui o ponto por vírgulas
+//multiplica o valor da variável 'moeda'
 var valorEmReal = (moeda * dolarHoje)
 var valorEmEuro = (moeda * eurHoje)
 var valorEmBtc = (moeda * btcHoje)
 
+
+//Pega o valor atual das variaveis objetos da API, multiplica pelo valor inserido pelo usuário e usa o LocalString para formatar o resultado da operação. Fixa os valores em no máixmo duas casas decimais
 var valorEmReal =  
 document.getElementById('resultado-dolar').value= "R$" + valorEmReal.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
 var valorEmEuro =  
@@ -38,11 +39,12 @@ document.getElementById('resultado-euro').value= "R$" + valorEmEuro.toLocaleStri
 var valorEmBtc =  
 document.getElementById('resultado-btc').value= "R$" + valorEmBtc.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
 
-    //exibe a cotação após a conversão
+    //exibe a cotação em valor unitário de 1(um) no topo da página após a conversão
     var divCotacaoHoje = document.getElementById("valor-atual")
     var htmlDolar = "Dólar:" + dolarHoje
     var htmlEuro = " Euro:" + eurHoje
     var htmlBitcoin = " BTC:" + btcHoje
-  
+
+    //Escreve no html os valores atribuídos acima
     divCotacaoHoje.innerHTML = htmlDolar + htmlEuro + htmlBitcoin 
 }
