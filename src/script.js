18b12c03c26924899bde9707fd4d2d5069e2e845
DanielTolentino <danieltolentino@protonmail.com>
2023-01-25 14:33:06 -0300

    blame me: one commit for a entire refactor

--- src/script.js
+++ src/script.js
@@ -1,48 +1,54 @@
-//API para consultar valor do dólar via fetch
-const URL_TO_FETCH = 'https://economia.awesomeapi.com.br/json/all/USD-BRL,EUR-BRL,BTC-BRL';
-fetch(URL_TO_FETCH, {
-  method: 'get' //required
-})
-.then(function(response) {
-  
-    response.json()
+async function fetchData() {
+  const URL_TO_FETCH = 'https://economia.awesomeapi.com.br/json/all/USD-BRL,EUR-BRL,BTC-BRL';
+  try {
+    const response = await fetch(URL_TO_FETCH);
+    const result = await response.json();
+    dolarHoje = result.USD["ask"];
+    eurHoje = result.EUR["ask"];
+    btcHoje = result.BTC["ask"];
+    displayValues();
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-    .then(function(result) {
-     dolarHoje = result.USD["ask"]
-     eurHoje = result.EUR["ask"]
-     btcHoje = result.BTC["ask"]
-  
-    })
-})
-.catch(function(err) { 
-  console.error(err);
-});
+fetchData();
 
-//Cria a função para multiplicar o valor em real por dólar, e mostar na tela o resultado
-function converter(){
-       //recebe o input digitado pelo usuário no HTML
-var moeda = document.getElementById('entrada').value
-//multiplica o valor inserido pelo usuário para corrigir o display das casas decimais no BTC
-let btcRate = moeda * 1000;
-//multiplica o valor da variável 'moeda'
-var valorEmReal = (moeda * dolarHoje)
-var valorEmEuro = (moeda * eurHoje)
-var valorEmBtc = (btcRate * btcHoje)
-
-//Pega o valor atual das variaveis objetos da API, multiplica pelo valor inserido pelo usuário e usa o LocalString para formatar o resultado da operação. Fixa os valores em no máixmo duas casas decimais
-var valorEmReal =  
-document.getElementById('resultado-dolar').value= "R$" + valorEmReal.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-var valorEmEuro =  
-document.getElementById('resultado-euro').value= "R$" + valorEmEuro.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-var valorEmBtc =  
-document.getElementById('resultado-btc').value= "R$" + valorEmBtc.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
-
-    //exibe a cotação em valor unitário de 1(um) no topo da página após a conversão
-    var divCotacaoHoje = document.getElementById("valor-atual")
-    var htmlDolar = "Dólar:" + dolarHoje
-    var htmlEuro = " Euro:" + eurHoje
-    var htmlBitcoin = " BTC:" + btcHoje
+function displayValues() {
+    let divCotacaoHoje = document.getElementById("valor-atual")
+    let htmlDolar = "Dólar:" + dolarHoje
+    let htmlEuro = " Euro:" + eurHoje
+    let htmlBitcoin = " BTC:" + btcHoje
 
     //Escreve no html os valores atribuídos acima
-    divCotacaoHoje.innerHTML = htmlDolar + htmlEuro + htmlBitcoin 
+    divCotacaoHoje.innerHTML = htmlDolar + htmlEuro + htmlBitcoin
+}
+
+let inputValue;
+let valorEmReal;
+let valorEmEuro;
+let valorEmBtc;
+
+function getInput() {
+         //recebe o input digitado pelo usuário no HTML
+         inputValue = document.getElementById('entrada').value;
+         if(!inputValue) return; // return if inputValue is empty or not a number
+}
+
+function multiply() {
+    //multiplica o valor inserido pelo usuário para corrigir o display das casas decimais no BTC
+    let btcRate = inputValue * 1000;
+    //multiplica o valor da variável 'moeda'
+    valorEmReal = (inputValue * dolarHoje)
+    valorEmEuro = (inputValue * eurHoje)
+    valorEmBtc = (btcRate * btcHoje)
+  
+}
+function converter(){
+  let input = getInput();
+  multiply();
+  document.getElementById('resultado-dolar').value = "R$" + valorEmReal.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+  document.getElementById('resultado-euro').value = "R$" + valorEmEuro.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+  document.getElementById('resultado-btc').value = "R$" + valorEmBtc.toLocaleString({style: 'currency', currency: 'BRL'}, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+
 }
